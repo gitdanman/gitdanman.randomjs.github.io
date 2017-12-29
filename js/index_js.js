476e14f1faeaeb8b7c9f1489bd033dd3eb230a34
gitdanman <32048280+gitdanman@users.noreply.github.com>
2017-12-29 12:42:00 -0500

    Update index_js.js
    added comments and two functions(circumference, square root)

--- js/index_js.js
+++ js/index_js.js
@@ -1,13 +1,17 @@
 var input = document.querySelector('.userNumber');
 var para = document.querySelector('p');
 
+// squares users number
 function squared(num) {
   return num * num;
 }
+
+// cubes users number
 function cubed(num) {
   return num * num * num;
 }
 
+// returns the factorial of the user's number
 function factorial(num) {
   var x = num;
   while (x > 1) {
@@ -17,6 +21,17 @@ function factorial(num) {
   return num;
 }
 
+// returns the circumference if the user's number is the radius
+function circum(num) {
+  return 2 * Math.PI * num;
+}
+
+// returns the square root of the user's number
+function sqrt(num) {
+  return Math.sqrt(num);
+}
+
+// writes all the functions out and checks if user input is a number
 input.onchange = function() {
   var num = input.value;
 
@@ -25,6 +40,8 @@ input.onchange = function() {
   } else {
     para.textContent = num + ' squared is ' + squared(num) + '. ' +
                        num + ' cubed is ' + cubed(num) + '. ' +
-                       num + ' factorial is ' + factorial(num) + '.';
+                       num + ' factorial is ' + factorial(num) + '. ' +
+                       num + ' circumference based on radius is ' + circum(num) + '. ' +
+                       num + ' square root is ' + sqrt(num) + '.';
   }
 }
